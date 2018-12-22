95c032fce27321f2fad7c3361c862358d24ad971
wyu <kharma@gmail.com>
2018-12-22 13:06:09 -0800

    add script

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# reference: https://hub.docker.com/_/ruby/
+# echo "==> Running docker pull"
+# docker pull ruby
+# docker run -it ruby
+
+# reference: https://hub.docker.com/_/ubuntu/
+# echo "==> Running docker pull"
+# docker pull ubuntu
+docker run -it ubuntu

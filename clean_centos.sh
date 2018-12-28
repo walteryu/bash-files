2323976b3d372a38df55bf0d8b29d1d6137a737a
wyu <kharma@gmail.com>
2018-12-28 10:52:47 -0800

    update script

--- clean_centos.sh
+++ clean_centos.sh
@@ -1,29 +1,11 @@
-#!/bin/bash -eux
-
-echo "==> Clean up yum cache of metadata and packages to save space"
-yum -y --enablerepo='*' clean all
-
-echo "==> Removing temporary files used to build box"
-rm -rf /tmp/*
-
-echo "==> Rebuild RPM DB"
-rpmdb --rebuilddb
-rm -f /var/lib/rpm/__db*
-
-# delete any logs that have built up during the install
-find /var/log/ -name *.log -exec rm -f {} \;
-
-# clear memory
-sync
-
-echo "==> Disk usage before cleanup"
-echo "${DISK_USAGE_BEFORE_CLEANUP}"
-
-echo "==> Disk usage after cleanup"
-df -h
+#!/bin/bash
 
 # clean all packages:
+echo "==> Cleaning packages"
 yum clean packages
+echo "==> Cleaning metadata"
 yum clean metadata
+echo "==> Cleaning headers"
 yum clean headers
+echo "==> Cleaning all"
 yum clean all

cffdc7d3624febd4bba3bc4b427eeea7e4e64908
wyu <kharma@gmail.com>
2019-01-03 07:13:21 -0800

    update script

--- docker.sh
+++ docker.sh
@@ -8,4 +8,13 @@
 # reference: https://hub.docker.com/_/ubuntu/
 # echo "==> Running docker pull"
 # docker pull ubuntu
-docker run -it ubuntu
+# docker run -it ubuntu
+
+# reference: https://www.tensorflow.org/install/docker
+# docker pull tensorflow/tensorflow
+# docker run -it -p 8888:8888 tensorflow/tensorflow
+
+# reference: https://quay.io/repository/azavea/raster-vision
+# docker pull quay.io/azavea/raster-vision:cpu-0.8
+# docker pull quay.io/azavea/raster-vision:gpu-0.8
+docker run --rm -it quay.io/azavea/raster-vision:cpu-0.8 /bin/bash

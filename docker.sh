7173b6659e5299565081db4d81c473875e32481f
kharma <kharma@gmail.com>
2019-01-05 13:33:39 -0800

    update script

--- docker.sh
+++ docker.sh
@@ -15,8 +15,8 @@
 # docker run -it -p 8888:8888 tensorflow/tensorflow
 
 # reference: https://quay.io/repository/azavea/raster-vision
-# docker pull quay.io/azavea/raster-vision:cpu-0.8
 # docker pull quay.io/azavea/raster-vision:gpu-0.8
+# docker pull quay.io/azavea/raster-vision:cpu-0.8
 # docker run --rm -it quay.io/azavea/raster-vision:cpu-0.8 /bin/bash
 
 docker run --rm -it -p 6006:6006 \
